7443749df796056040c962efc0e24ea4c678750b
MR X MIRROR <127676677+MR-X-MIRROR-BOTZ@users.noreply.github.com>
2023-04-23 19:13:30 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -70,5 +70,6 @@ class _BotCommands:
         self.EvalCommand = f'eval{CMD_SUFFIX}'
         self.ExecCommand = f'exec{CMD_SUFFIX}'
         self.ClearLocalsCommand = f'clearlocals{CMD_SUFFIX}'
+        self.DriveCleanCommand = getCommand(f'DRIVECLEAN_COMMAND', f'driveclean{CMD_SUFFIX}')
 
 BotCommands = _BotCommands()
